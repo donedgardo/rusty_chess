65afc1b8a980d00769260c290ecf054d10743e9e
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2024-07-21 00:20:30 -0400

    Add bevy and ci stuff

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,24 @@ mod board_position;
 mod game;
 mod pieces;
 
+use bevy::asset::AssetMetaCheck;
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+            meta_check: AssetMetaCheck::Never,
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("ducky.png"),
+        ..Default::default()
+    });
 }
